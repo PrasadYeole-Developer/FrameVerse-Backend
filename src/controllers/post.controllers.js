b200f41587b1176ea2e51abb8ed61c5543e93eec
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-12-05 15:27:59 +0530

    Setting up postController with multer for image file upload

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -1,7 +1,8 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/user.model");
+const Post = require("../models/post.model");
 require("dotenv").config();
 
-const postsControllers = async (req, res) => {};
+const postsControllers = async (req, res) => {
+  const file = req.file;
+};
 
 module.exports = { postsControllers };
