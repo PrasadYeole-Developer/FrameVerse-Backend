16835527a03e7c363a577558ccd398cc6392188b
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-12-09 22:33:45 +0530

    post schema ref error fix

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -1,6 +1,6 @@
 const { mongoose } = require("mongoose");
 const postSchema = require("../schemas/post.schema");
 
-const postModel = new mongoose.model("Post", postSchema);
+const postModel = new mongoose.model("post", postSchema);
 
 module.exports = postModel;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 const { mongoose } = require("mongoose");
 const userSchema = require("../schemas/user.schema");
 
-const UserModel = new mongoose.model("User", userSchema);
+const UserModel = new mongoose.model("user", userSchema);
 
 module.exports = UserModel;
