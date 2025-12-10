9f8f68450d5258fd0a77ce78d59a6c0dab49774d
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-12-10 14:55:35 +0530

    Fixed cors for frontend api call fix

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://frameverse-eta.vercel.app/",
+    origin: "https://frameverse-eta.vercel.app",
     credentials: true,
   })
 );
